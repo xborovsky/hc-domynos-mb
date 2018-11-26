919a93e7a1d9779c9a7171b2b76a7d85c4ad1929
Marek Borovsky <borovsky@oksystem.cz>
2018-11-26 06:14:18 +0100

    Finish Loader

--- src/components/common/loader/index.js
+++ src/components/common/loader/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import './Loader.css';
 
-
-const Loader = () =>
-    <CircularProgress className="loader" style={{color: '#182547'}} />
+const Loader = ({className, ...rest}) =>
+    <CircularProgress className={classnames("loader", className)} {...rest} />
 ;
+
+Loader.propTypes = {
+    className : PropTypes.string
+}
 
 export default Loader;
